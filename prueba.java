9b3d3ba3e033c81908db9637b6b45171b55e1c2e
ElSeeR <sergio9garcia9@gmail.com>
2020-10-02 13:56:37 +0200

    Añadido mi nombre en la 5a linea

--- prueba.java
+++ prueba.java
@@ -5,6 +5,7 @@ public class MiPrimeraClase {
 		System.out.println("Sergio Bailez Garcia");
 		System.out.println("La Bañeza 2001");
 		System.out.println("Leon Windows 10");
+		System.out.println("Sergio Bailez Garcia");
 	}
 
 }
